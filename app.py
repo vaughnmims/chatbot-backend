cc96ab34fcf0d7aa8aab2913ea0e74d3fa125161
vaughnmims <vaughn@insurems.com>
2025-01-20 17:22:06 -0600

    Update app.py

--- app.py
+++ app.py
@@ -24,5 +24,5 @@ def chat():
 
 if __name__ == "__main__":
     import os
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
 
